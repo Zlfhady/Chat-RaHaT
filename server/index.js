567e85ee6006187b87b66841bac576d1cb71e970
Pemweb <zhadyk49@gmail.com>
2025-02-11 11:03:20 +0700

    modified index.js

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ app.get('/', (req, res) => {
     res.send('Hello, World! Backend running on Vercel!');
 });
 
+app.get("/api/messages", (req, res) => {
+  res.json([{ id: 1, text: "Hello from backend!" }]);
+});
+
 app.post('/', (req, res) => {
     const { message, user: sender, type, members } = req.body;
 
