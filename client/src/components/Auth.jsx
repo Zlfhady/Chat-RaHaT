389f393804d7e16dcf95295594bac84a5e414851
Pemweb <zhadyk49@gmail.com>
2025-02-11 15:20:59 +0700

    update .env

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,8 +28,9 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
-        // const URL = 'https://medical-pager.herokuapp.com/auth';
+        const API_URL = "https://server-nama-proyek.vercel.app";
+
+        
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
